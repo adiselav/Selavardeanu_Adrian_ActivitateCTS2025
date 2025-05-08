d6232f9d600cab8b030f21a36e011ec6e6c91659
Adi Sela <adisela2003@gmail.com>
2025-05-08 17:49:51 +0300

    seminar10

---
+++ Seminar10/Composite/src/ro/cts/main/Main.java
@@ -0,0 +1,29 @@
+package ro.cts.main;
+
+import ro.cts.models.Departament;
+import ro.cts.models.Sectie;
+import ro.cts.models.StructuraAbs;
+
+public class Main {
+    public static void main(String[] args) {
+        StructuraAbs d1 = new Departament("Chirurgie",10);
+        StructuraAbs d2 = new Departament("Pediatrie",20);
+        StructuraAbs d3 = new Departament("Dermatologie",15);
+
+        StructuraAbs s1 = new Sectie("chirurgie estetica", 25);
+        StructuraAbs s2 = new Sectie("chirurgie neuronala", 9);
+        StructuraAbs s3 = new Sectie("imagistica", 12);
+
+        d1.adaugaNod(d2);
+        d1.adaugaNod(s1);
+        d1.adaugaNod(s2);
+        d2.adaugaNod(s3);
+
+        System.out.println(d1.calculeazaNrAngajati());
+        System.out.println("-----------------------");
+        System.out.println(d2.calculeazaNrAngajati());
+        System.out.println("-----------------------");
+        d1.afiseazaDescriere("");
+
+    }
+}
